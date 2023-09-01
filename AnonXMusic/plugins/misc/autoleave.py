f6a3c854772fad9189e1afb1c892be705ab01bd6
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-01 11:23:27 +0530

    Update autoleave.py

--- AnonXMusic/plugins/misc/autoleave.py
+++ AnonXMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1001626004802
+                                and i.chat.id != --1001754457302
                             ):
                                 if left == 20:
                                     continue
