9d70ebb6061ad12472b16f2b640546229ac56dc3
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-17 16:03:10 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -50,7 +50,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     elif 80 <= umm < 95:
         bar = "─────────▷─"
     else:
-        bar = "──────────▷""
+        bar = "──────────▷"
         
     buttons = [
         [
