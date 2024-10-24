23c3de71094dcacb3f4f9e8d38dbc752896b20a4
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-10-24 20:31:15 +0530

    Update mongo.py

--- SYSTUM/core/mongo.py
+++ SYSTUM/core/mongo.py
@@ -1,14 +1,35 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
 
-from config import MONGO_DB_URI
+import config
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("Connecting to your Mongo Database...")
-try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
-    LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
-    exit()
+TEMP_MONGODB = "mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority"
+
+DB_NAME = "Branded"
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning(
+        "No MONGO DB URL found.. Your Bot will work on Branded's Database"
+    )
+    temp_client = Client(
+        "Branded",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    mongodb = _mongo_async_[DB_NAME]
+    pymongodb = _mongo_sync_[DB_NAME]
