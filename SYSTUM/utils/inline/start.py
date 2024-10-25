02d019238b8dfc424eecdb3790c34d737296c4cc
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-10-25 10:53:28 +0530

    𝐒𝐘𝐒𝐓𝐔𝐌

--- SYSTUM/utils/inline/start.py
+++ SYSTUM/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.HEROKU_CHAT2),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.HEROKU_CHAT),
         ],
     ]
     return buttons
